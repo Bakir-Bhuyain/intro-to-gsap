83840421f6f0051db0623b433a1a19ea24996166
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-01-30 12:16:17 +0600

    why we need to use timelone and whats is effecient its mentioned in the code

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ gsap.from("h1",{
       delay:1,
       // stagger:0.3,
       //if we want the stagger to go with reverse then we will use the negative property like(stagger : -1;)
-      stagger:-1,
+      // stagger:-1,
 })
 
 gsap.to("#box3",{
@@ -33,7 +33,15 @@ gsap.to("#box3",{
       duration:2,
       delay:1,
       rotate:360,
-      repeat:-1,
+      // repeat:-1,
       // yoyo:true,
-      opacity:0,
+      // opacity:0,
+})
+
+
+//the use of the timeline is like when we work with different element in from same section where contain same element that is required to move one after one and thats's where we need to calculate the duration time to show the animation  in the webSite that's why we need timeline so that we do not need to calculate each one by each one , that's the reason why we need to use the timeline;
+var tl = gsap.timeline();
+
+tl.from("box2",{
+      stagger:-1,
 })
